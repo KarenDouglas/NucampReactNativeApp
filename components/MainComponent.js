c9eb7cecfb3617cf21fcacf4be4bf9db38a72115
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2021-06-03 14:52:09 -0500

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,8 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import  About from './AboutComponent';
 import Contact from "./ContactComponent";
 import Favorites from './FavoritesComponent';
+import Reservation from './ReservationComponent';
+import Login from './LoginComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -13,7 +15,6 @@ import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
-import Reservation from './ReservationComponent';
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -163,6 +164,28 @@ const FavoritesNavigator = createStackNavigator(
         })
     }
 );
+const LoginNavigator = createStackNavigator(
+    {
+        Login: { screen: Login }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='sign-in'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 
 const CustomDrawerContentComponent = props => (
@@ -185,6 +208,19 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
     {
+        Login: {
+            screen: LoginNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='sign-in'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },        
         Home: { 
             screen: HomeNavigator,
             navigationOptions: {
@@ -269,6 +305,7 @@ const MainNavigator = createDrawerNavigator(
         }
     },
     {
+        initialRouteName: 'Home',
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
     }
